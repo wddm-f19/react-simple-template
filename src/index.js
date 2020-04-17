60a7539e6df5ed0c601e8fa54f88ae39781aaf27
Rocco Panacci <rocco@Roccos-MacBook-Pro.local>
2020-04-17 12:29:07 -0400

    First route setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import HelloWorld from 'components/HelloWorld'
+
 import 'css/reset.css';
 import 'css/index.css';
 
 const App = () => {
 	return (
 		<>
-			<HelloWorld />
+			<header></header>
+			<Router>
+
+				<Route path="/">
+					<HelloWorld />
+				</Route>
+
+			</Router>
+			<footer></footer>
 		</>
 	)
 }
