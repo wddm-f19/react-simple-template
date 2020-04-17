1d8cac2081a947f4423ec8c3ad861b74c622c178
Rocco Panacci <rocco@Roccos-MacBook-Pro.local>
2020-04-17 12:52:56 -0400

    Top level router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 import Course from 'components/Course'
 import Lessons from 'components/Lessons'
@@ -10,16 +10,23 @@ import 'css/index.css';
 
 const App = () => {
 	return (
-		<>
-			<header></header>
-			<Router>
+		<Router>
+			<header>
+				<nav>
+					<ul>
+						<li><Link to="/">Course Home</Link></li>
+						<li><Link to="/lessons">Lessons</Link></li>
+					</ul>
+				</nav>
+			</header>
+			<main>
 
 				<Route exact path="/" component={Course} />
 				<Route path="/lessons" component={Lessons} />
 
-			</Router>
-			<footer></footer>
-		</>
+			</main>
+			<footer>Copyright 2020 &copy;</footer>
+		</Router>
 	)
 }
 
