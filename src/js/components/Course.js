9dabe4c294bd0bff7019b5a33a0299f71bb1c967
Rocco Panacci <rocco@Roccos-MacBook-Pro.local>
2020-04-17 12:46:57 -0400

    Using the Link component

--- src/js/components/Course.js
+++ src/js/components/Course.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Course = () => {
 	return (
 		<>
 			<h1>Welcome to WDDM 122</h1>
+			<ul>
+				<li><Link to="/lessons">View the lessons</Link></li>
+			</ul>
 		</>
 	)
 }
